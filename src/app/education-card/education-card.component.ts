e8bc214fb21f30486659447bc7ed6bb9f0f567c4
Yeongu <yeongu@student.ubc.ca>
2023-05-06 17:28:40 +0900

    1

--- src/app/education-card/education-card.component.ts
+++ src/app/education-card/education-card.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-education-card',
   templateUrl: './education-card.component.html',
   styleUrls: ['./education-card.component.css']
 })
 export class EducationCardComponent {
-  school = "University of British Columbia";
-  degree = "Bachelor of Science";
-  major = "Computer science";
+  tiles: Tile[] = [
+
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'One', cols: 3, rows: 2, color: 'lightblue'},
+  ];
 }
